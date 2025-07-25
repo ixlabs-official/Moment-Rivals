6e2ab10161fa858b3141970d69e8531c48a7b56d
justmacn <172618812+justmacn@users.noreply.github.com>
2025-07-25 16:39:17 -0400

    added base scaffolding for landing page

---
+++ frontend/src/components/global/Nav.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav>
+      {/* Logo Left */}
+      <div>Logo</div>
+
+      {/* Twitter Login Button or User Info */}
+      <div>
+        {/* Conditional: Show "Continue with Twitter" or Profile */}
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
